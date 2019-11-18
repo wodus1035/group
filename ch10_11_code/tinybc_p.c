0880b7a232514e13ff485a6f9aaa8496d509d89e
wodus1035 <wkddus962naver.com>
2019-11-18 17:49:24 +0900

    ch10

--- ch10_11_code/tinybc_p.c
+++ ch10_11_code/tinybc_p.c
@@ -13,19 +13,38 @@ int main()
 {
 	int	pid, todc[2], fromdc[2];	/*equipment	*/
 
-    //todo
+	if(pipe(todc) == -1 || pipe(fromdc) == -1)
+		oops("pipe failed", 1);
 
 	// test
 	printf("todc[0]  : %d, todc[1]  : %d \n", todc[0], todc[1]);
 	printf("fromdc[0]: %d, fromdc[1]: %d\n", fromdc[0], fromdc[1]);
 
 	/* get a process for user interface */
-    //todo
+	if((pid = fork()) == -1)
+		oops("cannot fork", 2);
+	if(pid == 0)
+		be_dc(todc,fromdc);
+	else{
+		be_bc(todc,fromdc);
+		wait(NULL);
+	}	
 }
 
 void be_dc(int in[2], int out[2])
 {
-    //todo
+	if(dup2(in[0],0) == -1)
+		oops("dc : annot redirect stdin", 3);
+	close(in[0]);
+	close(in[1]);
+	
+	if(dup2(out[1],1) == -1)
+		oops("dc: cannot redirect stdout", 4);
+	close(out[1]);
+	close(out[0]);
+	
+	execlp("dc", "dc", "-", NULL);
+	oops("Cannot run dc",5);
 }
 
 
@@ -36,14 +55,33 @@ void be_bc( int todc[2], int fromdc[2])
 	FILE	*fpout, *fpin, *fdopen();
 
 	/* setup */
-    //todo
-    
-	if( fpout == NULL || fpin == NULL )
+   	close(todc[0]);
+	close(fromdc[1]);
+	
+	fpout = fdopen(todc[1], "w");
+	fpin = fdopen(fromdc[0], "r");
+	
+	if(fpout == NULL || fpin == NULL)
 		fatal("Error converting pipes to streams");
 
-
 	/* main loop */
-    //todo
+	while(printf("tinybc:"), fgets(message, BUFSIZ, stdin) != NULL )
+	{
+		if(sscanf(message, "%d%[=+*/^]%d", &num1, operation, &num2) !=3 ){
+			printf("syntax error\n");
+			continue;
+		}
+		
+		if(fprintf(fpout, "%d\n%d\n%c\np\n", num1, num2, *operation) == EOF)
+			fatal("Error writing");
+		fflush(fpout);
+		if(fgets(message, BUFSIZ, fpin) == NULL)
+			break;
+		printf("%d %c %d = %s", num1, *operation, num2, message);
+	}
+	fclose(fpout);
+	fclose(fpin);
+
 }
 
 void fatal( char mess[] )
