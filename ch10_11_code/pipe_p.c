c10fc36ba1edd89a279683dcc43b5dddb06f028f
wodus1035 <wkddus962naver.com>
2019-11-18 15:53:57 +0900

    ch10

--- ch10_11_code/pipe_p.c
+++ ch10_11_code/pipe_p.c
@@ -25,6 +25,25 @@ main(int ac, char **av)
 		fprintf( stderr, "usage : pipe cmd1 cmd2\n" );
 		exit(1);
 	}
-    //todo
+	
+	if(pipe(thepipe) == -1)
+		oops("Cannot get a pipe", 1);
+	if((pid=fork()) == -1)
+		oops("Cannot fork",2);
+	
+	if(pid>0){
+		close(thepipe[1]);
+		if(dup2(thepipe[0],0) == -1)
+			oops("could not redirect stdin",3);
+		close(thepipe[0]);
+		execlp(av[2],av[2],NULL);
+		oops(av[2],4);
+	}
 
+	close(thepipe[0]);
+	if(dup2(thepipe[1],1) == -1)
+		oops("could not redirect stdout",4);
+	close(thepipe[1]);
+	execlp(av[1], av[1], NULL);
+	oops(av[1],5);
 }
--- ch10_11_code/pipedemo2_p.c
+++ ch10_11_code/pipedemo2_p.c
@@ -14,11 +14,35 @@
 
 main()
 {
-    int pipefd[2];
+    	int pipefd[2];
 	int	len;			/* for write	*/
 	char	buf[BUFSIZ];		/* for read	*/
 	int	read_len;
 	    
-    //todo
+	if ( pipe (pipefd) == -1)
+		oops("cannot get a pipe", 1);
+	switch(fork()){
+		case -1:
+			oops("cannot fork",2);
+		case 0:
+			len = strlen(CHILD_MESS);
+			while(1){
+				if(write(pipefd[1],CHILD_MESS,len) != len)
+					oops("write",3);
+				sleep(5);
+			}
+		default:
+			len = strlen(PAR_MESS);
+			while(1){
+				if(write(pipefd[1], PAR_MESS, len) != len)
+					oops("write",4);
+				sleep(1);
+				read_len = read(pipefd[0], buf, BUFSIZ);
+				if(read_len<=0) 
+					break;
+				write(1,buf,read_len);
+			}
+		}
+	
 }
 
